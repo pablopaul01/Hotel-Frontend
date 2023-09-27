7d6a645272481fb342596be7da2c7c357ed553e2
Franco Giacobbe <frandg47@gmail.com>
2023-09-27 18:33:22 -0300

    agregando validaciones

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,29 +1,58 @@
 import React from 'react'
 import "../css/register.css"
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { REGISTRO_SCHEMA } from '../helpers/validationsSchemas'
+
 
 const Register = () => {
+
+    const { register, handleSubmit, formState: { erros }, reset } = useForm({
+        resolver: yupResolver(REGISTRO_SCHEMA)
+    })
+
+    const onSubmit = (data) => {
+        console.log(data);
+        reset();
+    }
+
     return (
-        <form className="text-white">
+        <form className="text-white" onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-2 pt-2">
+                <label className="form-label">Nombre Completo</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    name="name"
+                    {...register("name")}
+                />
+            </div>
             <div className="mb-2 pt-2">
                 <label className="form-label">Correo electrónico</label>
                 <input
                     type="email"
                     className="form-control"
-                    required />
+                    name="username"
+                    {...register("username")}
+                />
             </div>
             <div className="mb-2 pt-2">
                 <label className="form-label">Contraseña</label>
                 <input
                     type="password"
                     className="form-control"
-                    required />
+                    name="password"
+                    {...register("password")}
+                />
             </div>
             <div className="mb-2 pt-2">
                 <label className="form-label">Repetir Contraseña</label>
                 <input
                     type="password"
                     className="form-control"
-                    required />
+                    name="repassword"
+                    {...register("repassword")}
+                />
                 <small className="text-secondary">La contraseña debe tener al entre 8 y 16 caracteres, al menos
                     un dígito, al menos una minúscula y al menos una
                     mayúscula.</small>
