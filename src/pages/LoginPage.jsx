bd283345c679c8c09e3db2a25760f6b55309fdcc
Franco Giacobbe <frandg47@gmail.com>
2023-09-27 11:31:30 -0300

    cambio de logo

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,9 +8,9 @@ const LoginPage = () => {
                 <div className="col-lg-5 col-md-7 col-sm-9 col-xs-10 px-5 shadow div-login">
                     <div className="titulo border border-top-0 border-end-0 border-start-0">
                         <div>
-                            <h2 className="fs-1 titulo text-center d-block">Iniciar Sesión</h2>
+                            <h2 className="fs-1 titulo text-center d-block ms-5">Iniciar Sesión</h2>
                         </div>
-                        <div><img src='../../public/logoHotel.png' className="img-fluid ms-5 w-75" alt="logo-Rolling-Hotel" /></div>
+                        <div><img src='../../public/RollingHotel-transparente.png' className="img-fluid ms-5 w-75" alt="logo-Rolling-Hotel" /></div>
                     </div>
                     <Login />
                 </div>
