6bb627a4cfedfb6fbd2ebc774d479f4927c177b5
Franco Giacobbe <frandg47@gmail.com>
2023-09-26 21:39:09 -0300

    creo page de registro

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+    return (
+        <form classNameName="text-white">
+            <div className="mb-2 pt-2">
+                <label className="form-label">Nombre</label>
+                <input
+                    type="text"
+                    className="form-control" />
+            </div>
+            <div className="mb-2 pt-2">
+                <label className="form-label">Apellido</label>
+                <input
+                    type="text"
+                    className="form-control" />
+            </div>
+            <div className="mb-2 pt-2">
+                <label className="form-label">Correo electrónico</label>
+                <input
+                    type="email"
+                    className="form-control" />
+            </div>
+            <div className="mb-2 pt-2">
+                <label className="form-label">Contraseña</label>
+                <input
+                    type="password"
+                    className="form-control" />
+            </div>
+            <div className="mb-2 pt-2">
+                <label className="form-label">Repetir Contraseña</label>
+                <input
+                    type="password"
+                    className="form-control" />
+                <small className="text-secondary">La contraseña debe tener al entre 8 y 16 caracteres, al menos
+                    un dígito, al menos una minúscula y al menos una
+                    mayúscula.</small>
+            </div>
+
+            <div className="mb-1">
+                <input
+                    type="checkbox"
+                    className="form-check-input"
+                    required />
+                <label className="form-check-label">
+                    <small>Acepto términos y condiciones.
+                        <a href="./404.html" className="link">Ver</a>
+                    </small>
+                </label>
+            </div>
+
+            <div className="d-grid">
+                <button className="btn btn-danger boton-login" type="submit">Registrarme</button>
+            </div>
+            <div className="mt-3 text-center" id="btn-registro">
+                <span>¿Ya tienes una cuenta?
+                    <a href="#" className="btn link">Iniciar Sesión</a></span>
+            </div>
+        </form>
+    )
 }
 
 export default Register
--- src/pages/RegisterView.jsx
+++ src/pages/RegisterView.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Register from '../components'
 
 const RegisterView = () => {
   return (
-    <div>RegisterView</div>
+    <div>
+        <Register />
+    </div>
   )
 }
 
