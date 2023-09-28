5ee4ac04377ff32018a1f09d7b31eff0bc03e1c0
Franco Giacobbe <frandg47@gmail.com>
2023-09-28 08:53:19 -0300

    arreglando responsive de galeria

--- src/components/GaleryRecepcion.jsx
+++ src/components/GaleryRecepcion.jsx
@@ -4,7 +4,7 @@ import "../css/galeryRecepcion.css"
 const GaleryRecepcion = () => {
     return (
         <>
-            <div className='mb-5 mt-5'>
+            <div className='mb-5 mt-5 titulos-galery'>
                 <h4 className='text-secondary text-center categoria'>ÁREAS PÚBLICAS</h4>
                 <h2 className='text-dark text-center titulo-galery'>Recepción</h2>
             </div>
@@ -12,15 +12,15 @@ const GaleryRecepcion = () => {
                 <div className="col-lg-4 mb-4">
                     <img src="https://images.unsplash.com/photo-1621293954908-907159247fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
                     <img src="https://images.unsplash.com/photo-1546967900-1bea5f16b69d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt=""
-                        className="w-100 mb-4 rounded img-fluid img-galeria" />
+                        className="w-100 rounded img-fluid img-galeria" />
                 </div>
                 <div className="col-lg-4 mb-4">
                     <img src="https://images.unsplash.com/photo-1565031491910-e57fac031c41?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
-                    <img src="https://images.unsplash.com/photo-1572786258684-9b3d5671e7d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
+                    <img src="https://images.unsplash.com/photo-1572786258684-9b3d5671e7d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 rounded img-fluid img-galeria" />
                 </div>
                 <div className="col-lg-4 mb-4">
                     <img src="https://images.unsplash.com/photo-1621293954908-907159247fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
-                    <img src="https://images.unsplash.com/photo-1546967900-1bea5f16b69d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
+                    <img src="https://images.unsplash.com/photo-1546967900-1bea5f16b69d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 rounded img-fluid img-galeria" />
                 </div>
             </div>
 
--- src/components/GalerySolarium.jsx
+++ src/components/GalerySolarium.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GalerySolarium = () => {
     return (
         <>
-            <div className='mb-5'>
+            <div className='mb-5 titulos-galery'>
                 <h4 className='text-secondary text-center categoria'>ÁREAS PÚBLICAS</h4>
                 <h2 className='text-dark text-center titulo-galery'>Solarium</h2>
             </div>
@@ -11,17 +11,17 @@ const GalerySolarium = () => {
                 <div className="col-lg-4 mb-4">
                     <img src="https://images.unsplash.com/photo-1606819422797-74627f4dac0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
                     <img src="https://images.unsplash.com/photo-1562805791-dbca2a22bba2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
-                    <img src="https://plus.unsplash.com/premium_photo-1661378912901-469ead6a3882?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
+                    <img src="https://plus.unsplash.com/premium_photo-1661378912901-469ead6a3882?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 rounded img-fluid img-galeria" />
                 </div>
                 <div className="col-lg-4 mb-4">
                     <img src="https://images.unsplash.com/photo-1529290130-4ca3753253ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
                     <img src="https://plus.unsplash.com/premium_photo-1661604009026-c9304a011526?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
-                    <img src="https://plus.unsplash.com/premium_photo-1678286771657-cf22aa97faf0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
+                    <img src="https://plus.unsplash.com/premium_photo-1678286771657-cf22aa97faf0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 rounded img-fluid img-galeria" />
                 </div>
                 <div className="col-lg-4 mb-4">
                     <img src="https://images.unsplash.com/photo-1651376589993-e0b37872f3e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
                     <img src="https://images.unsplash.com/photo-1503003378590-66f89f543bc6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
-                    <img src="https://images.unsplash.com/photo-1529290130-4ca3753253ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80" alt="" className="w-100 mb-4 rounded img-fluid img-galeria" />
+                    <img src="https://images.unsplash.com/photo-1529290130-4ca3753253ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80" alt="" className="w-100 rounded img-fluid img-galeria" />
                 </div>
             </div>
 
