4ded5ad7e2b71dbcba2d727627eda9610864f66d
Franco Giacobbe <frandg47@gmail.com>
2023-09-28 00:30:52 -0300

    creando primera galeria recepcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import LoginPage from './pages/LoginPage'
+import GaleryPage from './pages/GaleryPage'
+
 
 
 function App() {
@@ -7,7 +9,8 @@ function App() {
 
   return (
     <>
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <GaleryPage />
     </>
   )
 }
---
+++ src/pages/GaleryPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GaleryRecepcion from '../components/GaleryRecepcion'
+
+
+const GaleryPage = () => {
+  return (
+    <div>
+        <GaleryRecepcion />
+    </div>
+  )
+}
+
+export default GaleryPage
