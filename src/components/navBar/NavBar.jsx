7abe8e4bc252e2aee9d9c153e81aa2d3ec0b6be2
unknown <eliana_f90@hotmail.com>
2023-09-28 15:00:06 -0300

    se realizan cambios en el formato

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,40 +1,43 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import "../../assets/Fonts/Knockout.otf"
 import "./NavBar.css"
-
+import {FaBars} from 'react-icons/fa'
+​
 const NavBar = () => {
   return (
     <div>
-        <Navbar expand="lg" className="navbar-container">
-      <Container fluid className='d-flex justify-content-between'>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll" className='d-flex justify-content-right'>
-          <Nav
-            className="ms-auto my-2 my-lg-0 "
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1" className='link-nav'>Inicio</Nav.Link>
-            <Nav.Link href="#action2" className='link-nav'>Galeria</Nav.Link>
-            <Nav.Link href="#action2" className='link-nav'>Reserva</Nav.Link>
-            <Nav.Link href="#action2" className='link-nav'>Quienes Somos</Nav.Link>
-            <Nav.Link href="#action2" className='link-nav'>Contacto</Nav.Link>
-            <Nav.Link href="#action2" className='link-nav'>Login</Nav.Link>
-            <Nav.Link href="#action2" className='link-nav'>Registro</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
+      <Navbar expand="lg" className="navbar-container p-0" sticky="top">
+        <Container fluid className='d-flex justify-content-between'>
+          <Navbar.Brand href="#" className='p-0'><img src="https://res.cloudinary.com/dcv6aut2v/image/upload/v1695793253/RollingHotel-transparente_vk9yak.png" alt="logo Rolling" style={{width:"140px"}}/></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span>
+            
+              <FaBars
+                color="#fff"
+              />
+            </span>
+          </Navbar.Toggle>
+          <Navbar.Collapse className='hamburger'>
+            <Nav
+              className="ms-auto my-2 my-lg-0 d-flex gap-3"
+            >
+              <span href="#action1" className='link-nav'>Inicio</span>
+              <span href="#action2" className='link-nav'>Galeria</span>
+              <span href="#action2" className='link-nav'>Reserva</span>
+              <span href="#action2" className='link-nav'>Quienes Somos</span>
+              <span href="#action2" className='link-nav'>Contacto</span>
+              <span href="#action2" className='link-nav'>Login</span>
+              <span href="#action2" className='link-nav'>Registro</span>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+​
     </div>
   )
 }
-
+​
 export default NavBar 
