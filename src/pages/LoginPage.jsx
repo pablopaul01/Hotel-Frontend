c5f5c0d2faf5a768bface8c60b5e1201232d903c
Franco Giacobbe <frandg47@gmail.com>
2023-09-28 00:41:07 -0300

    solucionando problemas con css

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import Login from '../components/Login'
 
 const LoginPage = () => {
     return (
-        <div className="container-fluid shadow my-3">
+        <div className="container-fluid shadow my-3 login">
             <div className="row mb-1 justify-content-center">
                 <div className="col-lg-5 col-md-7 col-sm-9 col-xs-10 px-5 shadow div-login">
                     <div className="titulo border border-top-0 border-end-0 border-start-0">
