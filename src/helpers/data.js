9e8088cd4ba903a39d59a2f8f0c055160bc80bed
pablopaul01 <juanpablosalomon@gmail.com>
2023-09-30 20:46:27 -0300

    se arreglan problemas con el crud de usuarios

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -1,38 +1,234 @@
 export const rooms = [
     {
         id: 1,
-        title: "DOBLE TWIN STANDARD",
-        imagen: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-        descripcion: "",
+        title: "DOBLE TWIN STANDARD BLUE",
+        imagen: [
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 1
+            },
+            {
+                img: "https://images.unsplash.com/photo-1631049421450-348ccd7f8949?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 2
+            },
+            {
+                img: "https://images.unsplash.com/photo-1619128395560-8a749ac9926d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+                id: 3
+            },
+            {
+                img: "https://images.unsplash.com/photo-1613553474179-e1eda3ea5734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 4
+            },
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 5
+            }],
+        descripcion: "Sus habitaciones cuentan con TV por cable; cama sommier en todos los cuartos; teléfono con DDI/DDN; secador de Cabello; servicio de cuna; baño privado en cada cuarto; calefacción central; cofre de seguridad con medidas especiales para notebooks hasta 16”",
         tipo: "DOBLE TWIN STANDARD",
         precio: 10000,
         disponibilidad: true
     },
     {
         id: 2,
-        title: "DOBLE MATRIMONIAL",
-        imagen: "https://images.unsplash.com/photo-1631049421450-348ccd7f8949?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-        descripcion: "",
+        title: "DOBLE MATRIMONIAL RED",
+        imagen: [
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 1
+            },
+            {
+                img: "https://images.unsplash.com/photo-1631049421450-348ccd7f8949?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 2
+            },
+            {
+                img: "https://images.unsplash.com/photo-1619128395560-8a749ac9926d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+                id: 3
+            },
+            {
+                img: "https://images.unsplash.com/photo-1613553474179-e1eda3ea5734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 4
+            },
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 5
+            }],
+        descripcion: "Las habitaciones disponen de ropa de cama de colores vivos y suelo de moqueta de color beige. Todas están equipadas con TV por cable, teléfono y baño privado. En las zonas comunes hay Wi-Fi gratuita.",
         tipo: "DOBLE MATRIMONIAL",
-        precio: 10000,
+        precio: 12000,
         disponibilidad: true
     },
     {
         id: 3,
-        title: "TRIPLE STANDARD",
-        imagen: "https://images.unsplash.com/photo-1619128395560-8a749ac9926d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
-        descripcion: "",
+        title: "TRIPLE STANDARD YELLOW",
+        imagen: [
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 1
+            },
+            {
+                img: "https://images.unsplash.com/photo-1631049421450-348ccd7f8949?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 2
+            },
+            {
+                img: "https://images.unsplash.com/photo-1619128395560-8a749ac9926d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+                id: 3
+            },
+            {
+                img: "https://images.unsplash.com/photo-1613553474179-e1eda3ea5734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 4
+            },
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 5
+            }],
+        descripcion: "Sus habitaciones cuentan con TV por cable; cama sommier en todos los cuartos; teléfono con DDI/DDN; secador de Cabello; servicio de cuna; baño privado en cada cuarto; calefacción central; cofre de seguridad con medidas especiales para notebooks hasta 16”",
         tipo: "TRIPLE STANDARD",
-        precio: 10000,
+        precio: 13000,
         disponibilidad: true
     },
     {
         id: 4,
-        title: "SUITE",
-        imagen: "https://images.unsplash.com/photo-1613553474179-e1eda3ea5734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-        descripcion: "SUITE",
+        title: "SUITE GREEN",
+        imagen: [
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 1
+            },
+            {
+                img: "https://images.unsplash.com/photo-1631049421450-348ccd7f8949?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 2
+            },
+            {
+                img: "https://images.unsplash.com/photo-1619128395560-8a749ac9926d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+                id: 3
+            },
+            {
+                img: "https://images.unsplash.com/photo-1613553474179-e1eda3ea5734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 4
+            },
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 5
+            }],
+        descripcion: "Las habitaciones disponen de ropa de cama de colores vivos y suelo de moqueta de color beige. Todas están equipadas con TV por cable, teléfono y baño privado. En las zonas comunes hay Wi-Fi gratuita.",
+        tipo: "SUITE",
+        precio: 20000,
+        disponibilidad: true
+    },
+    {
+        id: 5,
+        title: "TRIPLE STANDARD BLACK",
+        imagen: [
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 1
+            },
+            {
+                img: "https://images.unsplash.com/photo-1631049421450-348ccd7f8949?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 2
+            },
+            {
+                img: "https://images.unsplash.com/photo-1619128395560-8a749ac9926d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+                id: 3
+            },
+            {
+                img: "https://images.unsplash.com/photo-1613553474179-e1eda3ea5734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 4
+            },
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 5
+            }],
+        descripcion: "Las habitaciones del Hotel Plaza son cómodas y cuentan con TV de pantalla plana y baño privado. También incluyen caja fuerte. Hay servicio de lavandería.",
         tipo: "TRIPLE STANDARD",
+        precio: 14000,
+        disponibilidad: true
+    },
+    {
+        id: 6,
+        title: "TRIPLE STANDARD WHITE",
+        imagen: [
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 1
+            },
+            {
+                img: "https://images.unsplash.com/photo-1631049421450-348ccd7f8949?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 2
+            },
+            {
+                img: "https://images.unsplash.com/photo-1619128395560-8a749ac9926d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+                id: 3
+            },
+            {
+                img: "https://images.unsplash.com/photo-1613553474179-e1eda3ea5734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 4
+            },
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 5
+            }],
+        descripcion: "Las habitaciones del Internacional Hotel tienen suelos de moqueta y muebles de madera. Todas están equipadas con TV por cable, calefacción y baño privado.",
+        tipo: "SUITE",
+        precio: 20000,
+        disponibilidad: true
+    },
+    {
+        id: 7,
+        title: "TRIPLE STANDARD GRAY",
+        imagen: [
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 1
+            },
+            {
+                img: "https://images.unsplash.com/photo-1631049421450-348ccd7f8949?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 2
+            },
+            {
+                img: "https://images.unsplash.com/photo-1619128395560-8a749ac9926d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+                id: 3
+            },
+            {
+                img: "https://images.unsplash.com/photo-1613553474179-e1eda3ea5734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 4
+            },
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 5
+            }],
+        descripcion: "Las habitaciones del Hotel Plaza son cómodas y cuentan con TV de pantalla plana y baño privado. También incluyen caja fuerte. Hay servicio de lavandería.",
+        tipo: "DOBLE TWIN STANDARD",
         precio: 10000,
         disponibilidad: true
-    }
+    },
+    {
+        id: 8,
+        title: "TRIPLE STANDARD PURPLE",
+        imagen: [
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 1
+            },
+            {
+                img: "https://images.unsplash.com/photo-1631049421450-348ccd7f8949?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 2
+            },
+            {
+                img: "https://images.unsplash.com/photo-1619128395560-8a749ac9926d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+                id: 3
+            },
+            {
+                img: "https://images.unsplash.com/photo-1613553474179-e1eda3ea5734?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 4
+            },
+            {
+                img: "https://images.unsplash.com/photo-1605346576608-92f1346b67d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                id: 5
+            }],
+        descripcion: "Las habitaciones del Cambria disponen de ropa de cama de colores vivos y suelo de moqueta de color beige. Todas están equipadas con TV por cable, teléfono y baño privado. En las zonas comunes hay Wi-Fi gratuita.",
+        tipo: "DOBLE MATRIMONIAL",
+        precio: 13000,
+        disponibilidad: true
+    },
 ]
