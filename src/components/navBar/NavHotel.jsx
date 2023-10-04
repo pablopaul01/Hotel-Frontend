d5dcae539add3ad7975cbce2540c8e4419184e8e
pablopaul01 <juanpablosalomon@gmail.com>
2023-10-04 00:41:23 -0300

    se agregan cambios al navbar

--- src/components/navBar/NavHotel.jsx
+++ src/components/navBar/NavHotel.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useState,useRef} from 'react'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import "./NavBar.css"
 import {FaBars} from 'react-icons/fa'
+import NavbarLogin from '../NavbarLogin/NavbarLogin'
+
+
 
 const NavHotel = () => {
+  const [navbarFixed, setNavbarFixed] = useState(false);
+  const [shouldAppear, setShouldAppear] = useState(false);
+  const navbarRef = useRef();
+
+  const handleScroll = () => {
+    if (window.scrollY > navbarRef.current.offsetHeight) {
+      setNavbarFixed(true);
+      setShouldAppear(true);
+    } else {
+      setNavbarFixed(false);
+      setShouldAppear(false);
+    }
+  };
+
+  window.onscroll = handleScroll;
   return (
-    <div>
-      <Navbar expand="lg" className="navbar-container p-0" fixed="top">
-        <Container fluid className='d-flex justify-content-between'>
+    <div  ref={navbarRef}>
+      <NavbarLogin />
+      <Navbar expand="lg" className={shouldAppear ? 'appear navbar-container p-0' : 'navbar-container p-0'} fixed={navbarFixed ? 'top' : ''}>
+        <Container fluid className='d-flex justify-content-between '>
           <Navbar.Brand href="#" className='p-0'><img src="https://res.cloudinary.com/dcv6aut2v/image/upload/v1695793253/RollingHotel-transparente_vk9yak.png" alt="logo Rolling" style={{width:"140px"}}/></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span>
@@ -24,12 +43,18 @@ const NavHotel = () => {
               className="ms-auto my-2 my-lg-0 d-flex gap-4 me-3"
             >
               <span className='linknav'>Inicio</span>
-              <span className='linknav'>Galeria</span>
-              <span className='linknav'>Reserva</span>
+                  <li className="nav-item dropdown">
+                    <a className="dropdown-toggle linknav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Hotel
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li><a className="dropdown-item linknav" href="#">Galeria de imagenes</a></li>
+                      <li><a className="dropdown-item linknav" href="#">Nuestras Habitaciones</a></li>
+                    </ul>
+                  </li>
               <span className='linknav'>Quienes Somos</span>
               <span className='linknav'>Contacto</span>
-              <span className='linknav'>Login</span>
-              <span className='linknav'>Registro</span>
+              <span className='linknav'>Reserva</span>
             </Nav>
           </Navbar.Collapse>
         </Container>
